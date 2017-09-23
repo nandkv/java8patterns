0b30aa33a9118c6762e5e6762914b42b5ac1b50e
nandkv <nandkv.online@gmail.com>
2017-09-23 17:24:18 -0500

    cleanup

--- src/main/java/com/patterns/template/composition/App.java
+++ src/main/java/com/patterns/template/composition/App.java
@@ -1,5 +1,9 @@
 package com.patterns.template.composition;
 
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.SystemUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -27,5 +31,33 @@ public class App {
 		DelegateProcessor<AccountRequestDTO, AccountResponseDTO> ad = new DelegateProcessor<AccountRequestDTO, AccountResponseDTO>(accountDelegate, accountLogger);
 		accountLogger.debug(ad.process(new AccountRequestDTO()));
 
+		List<Customer> customerList = null;
+		if (CollectionUtils.isNotEmpty(customerList)){
+			for (Customer c : customerList){
+				//
+			}			
+		}
+		
+		String windows_filename = "c:\\view\\apps\\content\\fhct\\en\\balloon_help\\filename.json";
+		String unix_filename = "/opt/content/fhct/en/balloon_help/filename.json";
+		
+//		String[] arr = filename.split("\\.");
+//		
+//		for (String s: arr){
+//			System.out.println(s);
+//		}
+//		
+//		System.out.println(filename.split("\\.")[0].split("\\:")[1].replaceAll("\\\\", "_"));
+		
+		//if (SystemUtils.IS_OS_WINDOWS){
+			System.out.println(windows_filename.split("\\.")[0].split(":")[1].replaceAll("(\\\\|:|\\.|//)", "_"));
+		//} else {
+			//FileNameUtils.
+			System.out.println(unix_filename.split("\\.")[0].replaceAll("(\\\\|:|\\.|\\/)", "_"));
+		//}
+
+			String text = "this is a lt&number&gt";
+			String replaced = text.replace("number", "Nand");
+			System.out.println(replaced);
 	}
 }
